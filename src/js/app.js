607c44f02508a0b21eea176c9bfa691ec1525812
irfann <irfann.karimjy@gmail.com>
2014-11-06 11:56:51 +0100

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
 
 	var memory = {
+		check : [],
 		add : function(nb){
 			var tabRandom = [];
 			for (var i = 0; i < nb; i+=2) {
@@ -18,7 +19,7 @@ $(document).ready(function(){
 
 				$(document.createElement('img')).attr({
 					class:"memo",
-					src:"img/memory0" + tabRandom[i] + ".png"
+					src:"img/memory0" + tabRandom[i] + ".png",
 				}).prependTo(carte);
 			};
 		}, 
@@ -29,15 +30,43 @@ $(document).ready(function(){
 		},
 
 		game : function(event){
+			var that = this;
+			var cpt = 0;
 			$(".carte").click(function(event){
 				var event = event.currentTarget;
-				if($(event).find(".hide").is(":visible")){
-					$(event).find(".hide").hide();
-					$(event).find(".memo").show();
-				}else{
-					$(event).find(".hide").show();
-					$(event).find(".memo").hide();
-				}
+				cpt++;
+				if (that.check.length <= 2 ) {
+					if($(event).find(".hide").is(":visible")){
+						$(event).find(".hide").hide();
+						$(event).find(".memo").show();
+						that.check.push($(event));
+					}else{
+						$(event).find(".hide").show();
+						$(event).find(".memo").hide();
+						that.check.pop();
+					}
+				};
+
+				console.log($(that.check[0]).find(".memo").attr("src") +" "+$(that.check[1]).find(".memo").attr("src"));
+				
+				if(that.check.length == 2 && $(that.check[0]).find(".memo").attr("src") === $(that.check[1]).find(".memo").attr("src")){
+					console.log('gagner');
+					_.delay(function(){
+						$(that.check[0]).find(".hide").remove();
+						$(that.check[0]).find(".memo").remove();
+						$(that.check[1]).find(".hide").remove();
+						$(that.check[1]).find(".memo").remove();
+						that.check = [];
+					}, 1000);
+				}else if(that.check.length == 2 && $(that.check[0]).find(".memo").attr("src") != $(that.check[1]).find(".memo").attr("src")){
+					_.delay(function(){
+						$(that.check[0]).find(".hide").show();
+						$(that.check[0]).find(".memo").hide();
+						$(that.check[1]).find(".hide").show();
+						$(that.check[1]).find(".memo").hide();
+						that.check = [];
+					}, 1000);
+				};
 			});
 		}
 	};
